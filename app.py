0c26beca4944a34db7454e41bdc5a9f81557b6ee
emlin <emlincharly@gmail.com>
2023-08-30 11:50:51 -0400

    Adding comments and new page tempalte

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 
+# Create a Flask application instance
 app = Flask(__name__)
+# Set the static folder for serving static files like CSS, JavaScript, etc.
 app.static_folder = 'static'
+# Enable debugging mode, which provides detailed error messages
 app.debug = True
 
+# Define a route for the homepage ('/') that renders an HTML template
 @app.route('/')
-def waitListPage():
+def index():
+    """
+    Render the index.html template for the homepage.
+    This route is responsible for displaying the home page.
+    """
     return render_template('index.html')
+
+# Run the Flask application if this script is executed directly
+if __name__ == '__main__':
+    app.run()
--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+# Import the 'app' instance from the 'app' module (assuming 'app.py' contains your Flask app)
 from app import app
+# Import the 'os' module for environment variables
 import os
 
+# Check if this script is being run directly
 if __name__ == "__main__":
+    # Run the Flask application
+    # Listen on all available network interfaces (0.0.0.0) and use the port specified in the 'PORT' environment variable,
+    # defaulting to port 8888 if 'PORT' is not set
     app.run("0.0.0.0", port=os.getenv('PORT', 8888))
